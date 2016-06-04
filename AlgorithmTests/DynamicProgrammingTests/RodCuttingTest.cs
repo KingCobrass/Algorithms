4608f0f3af5babae7ae46e44f9d571a5180a96e0
Mike Moroney <mikemoroney1722@hotmail.com>
2016-06-03 19:34:13 -0700

    Added rod cutting algorithm and tests.

---
+++ AlgorithmTests/DynamicProgrammingTests/RodCuttingTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DynamicProgramming;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class RodCuttingTests
+    {
+        [TestMethod]
+        public void RodCuttingTest()
+        {
+            RodCuttingTests.RodCuttingTest(new int[] { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 });
+
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+                    int[] data = TestUtilities.GenerateRandomArray(j, 1, 10);
+                    int[] prices = new int[data.Length + 1];
+                    for (int k = 0; k < data.Length; k++)
+                        prices[k + 1] = prices[k] + data[k];
+
+                    RodCuttingTests.RodCuttingTest(prices);
+                }
+            }
+        }
+
+        private static void RodCuttingTest(int[] prices)
+        {
+            int[] expected = RodCuttingTests.MaximumRevenues(prices);
+            int[] actual = RodCutting.MaximumRevenues(prices);
+
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i]);
+
+            return;
+        }
+
+        private static int[] MaximumRevenues(int[] prices)
+        {
+            int[] revenues = new int[prices.Length];
+
+            for (int i = 1; i < prices.Length; i++)
+                revenues[i] = RodCuttingTests.MaximumRevenue(i, prices);
+
+            return revenues;
+        }
+
+        private static int MaximumRevenue(int length, int[] prices)
+        {
+            int revenue = prices[length];
+            for (int i = 1; i < length; i++)
+                revenue = Math.Max(revenue, prices[i] + RodCuttingTests.MaximumRevenue(length - i, prices));
+
+            return revenue;
+        }
+    }
+}
---
+++ Algorithms/DynamicProgramming/RodCutting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DynamicProgramming
+{
+    public static class RodCutting
+    {
+        public static int[] MaximumRevenues(int[] prices)
+        {
+            int[] revenues = new int[prices.Length];
+
+            for(int i = 1; i < prices.Length; i++)
+            {
+                revenues[i] = prices[i];
+
+                for(int j = 0; j < i; j++)
+                    revenues[i] = Math.Max(revenues[i], prices[j] + revenues[i - j]);
+            }
+
+            return revenues;
+        }
+    }
+}
