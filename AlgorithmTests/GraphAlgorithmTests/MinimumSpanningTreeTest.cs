7f9d0d9669e015ab4519cff4d26058a151d722cc
mmoroney <mikemoroney1722@hotmail.com>
2016-06-12 14:48:15 -0700

    Added Kruskal algorithm, partial test.

---
+++ AlgorithmTests/GraphAlgorithmTests/MinimumSpanningTreeTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.GraphAlgorithms;
+
+namespace AlgorithmTests.GraphAlgorithmTests
+{
+    [TestClass]
+    public class MinimumSpanningTreeTestClass
+    {
+        [TestMethod]
+        public void MinimumSpanningTreeTest()
+        {
+            int n = 10;
+            int[,] graph = MinimumSpanningTreeTestClass.CreateSpanningTree(n);
+            int[,] minimimSpanningTree = Kruskal.Run(graph);
+        }
+
+        private static int[,] CreateSpanningTree(int n)
+        {
+            int[,] graph = new int[n, n];
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    graph[i, j] = (i == j) ? 0 : int.MaxValue;
+            }
+
+            while (!MinimumSpanningTreeTestClass.IsSpanningTree(graph, n))
+                MinimumSpanningTreeTestClass.AddRandomWeightedEdge(graph, n, 10);
+
+            return graph;
+        }
+
+        private static void AddRandomWeightedEdge(int[,] graph, int n, int maxWeight)
+        {
+            bool found = false;
+            int i = 0;
+            int j = 0;
+
+            int count = 0;
+            Random random = new Random();
+
+            for (int k = 0; k < n; k++)
+            {
+                for (int l = k + 1; l < n; l++)
+                {
+                    if (graph[k, l] != int.MaxValue)
+                        continue;
+
+                    count++;
+
+                    if (random.Next(0, count) == 0)
+                    {
+                        found = true;
+                        i = k;
+                        j = l;
+                    }
+                }
+            }
+
+            if (found)
+                graph[i, j] = graph[j, i] = random.Next(1, maxWeight + 1);
+        }
+
+        private static bool IsSpanningTree(int[,] graph, int n)
+        {
+            bool[,] paths = new bool[n, n];
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    paths[i, j] = (graph[i, j] != int.MaxValue);
+            }
+
+            bool[] visited = DepthFirstSearch.Run(paths, 0);
+            return visited.All(b => b);
+        }
+    }
+}
---
+++ Algorithms/GraphAlgorithms/Kruskal.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.GraphAlgorithms
+{
+    public static class Kruskal
+    {
+        private class Edge
+        {
+            public int From { get; set; }
+            public int To { get; set; }
+            public int Weight { get; set; }
+        }
+
+        public static int[,] Run(int[,] graph)
+        {
+            int n = graph.GetLength(0);
+            if (n != graph.GetLength(1))
+                throw new ArgumentException(nameof(graph));
+
+            List<int>[] sets = new List<int>[n];
+            for(int i = 0; i < n; i++)
+            {
+                sets[i] = new List<int>();
+                sets[i].Add(i);
+            }
+
+            int[,] results = new int[n, n];
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                    results[i, j] = results[j, i] = int.MaxValue;
+            }
+
+            foreach(Edge edge in Kruskal.Edges(graph, n).OrderBy(e => e.Weight))
+            {
+                int from = edge.From;
+                int to = edge.To;
+                if(sets[from][0] != sets[to][0])
+                {
+                    results[from, to] = edge.Weight;
+                    results[to, from] = edge.Weight;
+
+                    foreach(int i in sets[to])
+                        sets[from].Add(i);
+
+                    sets[to] = sets[from];
+                }
+            }
+
+            return results;
+        }
+
+        private static IEnumerable<Edge> Edges(int[,] graph, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    int weight = graph[i, j];
+                    if (weight < int.MaxValue)
+                        yield return new Edge { From = i, To = j, Weight = weight };
+                }
+            }
+        }
+    }
+}
