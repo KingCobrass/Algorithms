8d8c55ba0857da011d49b94795873ef9af73c3d2
mmoroney <mikemoroney1722@hotmail.com>
2016-06-24 21:51:44 -0700

    Added all pairs shortest path test.

---
+++ AlgorithmTests/GraphAlgorithmTests/AllPairsShortestPathTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.GraphAlgorithms;
+
+namespace AlgorithmTests.GraphAlgorithmTests
+{
+    [TestClass]
+    public class AllPairsShortestPathTestClass
+    {
+        [TestMethod]
+        public void AllPairsShortestPathTest()
+        {
+            int n = 10;
+            int[,] graph = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    if (i != j)
+                        graph[i, j] = int.MaxValue;
+            }
+
+            for (int i = 0; i <= n * n; i++)
+            {
+                int[,] floydWarshall = FloydWarshall.Run(graph);
+                for(int j = 0; j < n; j++)
+                {
+                    int[] djikstra = Djikstra.Run(graph, j);
+
+                    for (int k = 0; k < n; k++)
+                        Assert.AreEqual(floydWarshall[j, k], djikstra[k]);
+                }
+
+                TestUtilities.SetRandomEdge(graph, n);
+            }
+        }
+    }
+}
