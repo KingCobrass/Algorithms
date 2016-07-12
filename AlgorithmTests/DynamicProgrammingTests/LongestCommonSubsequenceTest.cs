6cbe6338b1af430955b96acca8037c2eb6f537dc
mmoroney <mikemoroney1722@hotmail.com>
2016-07-12 13:20:21 -0700

    Added longest common subsequence problem.

---
+++ AlgorithmTests/DynamicProgrammingTests/LongestCommonSubsequenceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DynamicProgramming;
+
+namespace AlgorithmTests.DynamicProgrammingTests
+{
+    [TestClass]
+    public class LongestCommonSubsequenceTests
+    {
+        [TestMethod]
+        public void LongestCommonSubsequenceTest()
+        {
+            LongestCommonSubsequenceTests.LongestCommonSubsequenceTest("aaa", "aa", "aa");
+            LongestCommonSubsequenceTests.LongestCommonSubsequenceTest("abc", "a", "a");
+            LongestCommonSubsequenceTests.LongestCommonSubsequenceTest("aetr", "vbn", "");
+            LongestCommonSubsequenceTests.LongestCommonSubsequenceTest("abcdefgh", "aceg", "aceg");
+            LongestCommonSubsequenceTests.LongestCommonSubsequenceTest("abcbdab", "bdcaba", "bdab");
+        }
+
+        private static void LongestCommonSubsequenceTest(string a, string b, string expected)
+        {
+            string actual = LongestCommonSubsequence.GetLongestCommonSubsequence(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/DynamicProgramming/LongestCommonSubsequence.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DynamicProgramming
+{
+    public static class LongestCommonSubsequence
+    {
+        public static string GetLongestCommonSubsequence(string a, string b)
+        {
+            int[,] lengths = new int[a.Length + 1, b.Length + 1];
+
+            for(int i = 1; i <= a.Length; i++)
+            {
+                for(int j = 1; j <= b.Length; j++)
+                {
+                    if (a[i - 1] == b[j - 1])
+                        lengths[i, j] = lengths[i - 1, j - 1] + 1;
+                    else
+                        lengths[i, j] = Math.Max(lengths[i, j - 1], lengths[i - 1, j]);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            int k = a.Length;
+            int l = b.Length;
+
+            while (k > 0 && l > 0)
+            {
+                if (a[k - 1] == b[l - 1])
+                {
+                    sb.Insert(0, a[k - 1]);
+                    k--;
+                    l--;
+                }
+                else if (lengths[k, l - 1] < lengths[k - 1, l])
+                    k--;
+                else
+                    l--;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
