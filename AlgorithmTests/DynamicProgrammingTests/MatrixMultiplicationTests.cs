daefdf15525781dd1eca5fe533d9eed474f782c0
mmoroney <mikemoroney1722@hotmail.com>
2016-07-16 00:13:37 -0700

    Added Matrix Multiplication algorithm.

---
+++ AlgorithmTests/DynamicProgrammingTests/MatrixMultiplicationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DynamicProgramming;
+
+namespace AlgorithmTests.DynamicProgrammingTests
+{
+    [TestClass]
+    public class MatrixMultiplicationTests
+    {
+        [TestMethod]
+        public void MatrixMultiplicationTest()
+        {
+            MatrixMultiplicationTests.MatrixMultiplicationTest(new int[] { 1, 2 }, 0);
+            MatrixMultiplicationTests.MatrixMultiplicationTest(new int[] { 1, 2, 3 }, 6);
+            MatrixMultiplicationTests.MatrixMultiplicationTest(new int[] { 30, 35, 15, 5, 10, 20, 25 }, 15125);
+        }
+
+        private static void MatrixMultiplicationTest(int[] sizes, int expected)
+        {
+            int actual = MatrixMultiplication.MinimumOperations(sizes);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/DynamicProgramming/MatrixMultiplication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DynamicProgramming
+{
+    public static class MatrixMultiplication
+    {
+        public static int MinimumOperations(int[] sizes)
+        {
+            int count = sizes.Length - 1;
+            int[,] counts = new int[sizes.Length, sizes.Length];
+
+            for(int len = 2; len <= sizes.Length; len++)
+            {
+                for(int start = 0; start <= sizes.Length - len - 1; start++)
+                {
+                    int min = int.MaxValue;
+                    int end = start + len - 1;
+
+                    for (int mid = start; mid < end; mid++)
+                        min = Math.Min(min, counts[start, mid] + counts[mid + 1, end] + sizes[start] * sizes[mid + 1] * sizes[end + 1]);
+
+                    counts[start, end] = min;
+                }
+            }
+
+            return counts[0, count - 1];
+        }
+    }
+}
