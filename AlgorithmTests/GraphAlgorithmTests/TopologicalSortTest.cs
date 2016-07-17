ba62c95fea2d58b67176f9f49a2f0b5bad9c6d9e
mmoroney <mikemoroney1722@hotmail.com>
2016-07-17 15:55:52 -0700

    Added topological sort.

---
+++ AlgorithmTests/GraphAlgorithmTests/TopologicalSortTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.GraphAlgorithms;
+
+namespace AlgorithmTests.GraphAlgorithmTests
+{
+    [TestClass]
+    public class TopologicalSortTestClass
+    {
+        [TestMethod]
+        public void TopologicalSortTest()
+        {
+            int n = 10;
+
+            for(int i = 0; i < 10; i++)
+            {
+                bool[,] graph = TopologicalSortTestClass.CreateDirectedAcyclicGraph(n);
+
+                int[] order = TopologicalSort.Run(graph);
+
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    bool[] visited = DepthFirstSearch.Run(graph, order[j]);
+                    for (int k = 0; k < j; k++)
+                        Assert.IsFalse(visited[order[k]]);
+                }
+            }
+        }
+
+        private static bool[,] CreateDirectedAcyclicGraph(int n)
+        {
+            bool[,] graph = new bool[n, n];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if(i != j)
+                        graph[i, j] = true;
+                }
+            }
+
+            while (!TopologicalSortTestClass.IsDirectedAcyclicGraph(graph, n))
+                TopologicalSortTestClass.RemoveRandomEdge(graph, n);
+
+            return graph;
+        }
+
+        private static void RemoveRandomEdge(bool[,] graph, int n)
+        {
+            bool found = false;
+            int i = 0;
+            int j = 0;
+
+            int count = 0;
+            Random random = new Random();
+
+            for (int k = 0; k < n; k++)
+            {
+                for (int l = 0; l < n; l++)
+                {
+                    if (!graph[k, l])
+                        continue;
+
+                    count++;
+
+                    if (random.Next(0, count) == 0)
+                    {
+                        found = true;
+                        i = k;
+                        j = l;
+                    }
+                }
+            }
+
+            if (found)
+                graph[i, j] = false;
+        }
+
+        private static bool IsDirectedAcyclicGraph(bool[,] graph, int n)
+        {
+            bool[] visiting = new bool[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (TopologicalSortTestClass.HasCycle(graph, n, i, visiting))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static bool HasCycle(bool[,] graph, int n, int current, bool[] visiting)
+        {
+            if (visiting[current])
+                return true;
+
+            visiting[current] = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                if(graph[current, i] && TopologicalSortTestClass.HasCycle(graph, n, i, visiting))
+                    return true;
+            }
+
+            visiting[current] = false;
+
+            return false;
+        }
+    }
+}
---
+++ Algorithms/GraphAlgorithms/TopologicalSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.GraphAlgorithms
+{
+    public static class TopologicalSort
+    {
+        public static int[] Run(bool[,] graph)
+        {
+            int n = graph.GetLength(0);
+            if (n != graph.GetLength(1))
+                throw new ArgumentException(nameof(graph));
+
+            bool[] started = new bool[n];
+            bool[] finished = new bool[n];
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+                TopologicalSort.Search(graph, n, i, started, finished, list);
+
+            return list.ToArray();
+        }
+
+        private static void Search(bool[,] graph, int n, int current, bool[] started, bool[] finished, List<int> list)
+        {
+            if (started[current])
+                return;
+
+            started[current] = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (graph[current, i])
+                {
+                    if (started[i] && !finished[i])
+                        throw new ArgumentException(nameof(graph));
+
+                    TopologicalSort.Search(graph, n, i, started, finished, list);
+                }
+            }
+
+            finished[current] = true;
+            list.Insert(0, current);
+        }
+    }
+}
