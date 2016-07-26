7a6ed8c68cde68709417834d7ebb07e1b2ef994c
mmoroney <mikemoroney1722@hotmail.com>
2016-07-25 20:50:35 -0700

    Added maximum subarray problem.

---
+++ AlgorithmTests/MiscellaneousTests/MaximumSubarrayTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Miscellaneous;
+
+namespace AlgorithmTests.MiscellaneousTests
+{
+    [TestClass]
+    public class MaximumSubarrayTests
+    {
+        [TestMethod]
+        public void TestMaximumSubarray()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    int[] data = TestUtilities.GenerateRandomArray(j, -20, 20);
+
+                    int expected = MaximumSubarrayTests.CalculateMaximumSubarray(data);
+                    int actual = MaximumSubarray.Run(data);
+
+                    Assert.AreEqual(expected, actual);
+                }
+            }
+        }
+
+        private static int CalculateMaximumSubarray(int[] data)
+        {
+            int maxSum = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                for(int j = i; j < data.Length; j++)
+                {
+                    int sum = 0;
+
+                    for (int k = i; k <= j; k++)
+                        sum += data[k];
+
+                    maxSum = Math.Max(maxSum, sum);
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ Algorithms/Miscellaneous/MaximumSubarray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Miscellaneous
+{
+    public class MaximumSubarray
+    {
+        public static int Run(int[] data)
+        {
+            int currentSum = 0;
+            int maxSum = 0;
+
+            foreach(int i in data)
+            {
+                currentSum += i;
+                currentSum = Math.Max(currentSum, 0);
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+
+            return maxSum;
+        }
+    }
+}
