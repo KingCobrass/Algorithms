a392821c68ee0c2b72d373575e7e40ec09bbcc73
mmoroney <mikemoroney1722@hotmail.com>
2016-07-25 20:39:14 -0700

    Added Euclid's algorithm.

---
+++ AlgorithmTests/MathematicalTests/EuclidGCDTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Mathematical;
+
+namespace AlgorithmTests.MathematicalTests
+{
+    [TestClass]
+    public class EuclidGCDTests
+    {
+        [TestMethod]
+        public void TestGCD()
+        {
+            for(int a = 0; a < 100; a++)
+            {
+                for(int b = 0; b < 100; b++)
+                {
+                    int expected = EuclidGCDTests.CalculateGCD(a, b);
+                    int actual = EuclidGCD.Run(a, b);
+
+                    Assert.AreEqual(expected, actual);
+                }
+            }
+        }
+
+        private static int CalculateGCD(int a, int b)
+        {
+            for(int i = Math.Max(a, b); i > 0; i--)
+            {
+                if (a % i == 0 && b % i == 0)
+                    return i;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Algorithms/Mathematical/EuclidGCD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Mathematical
+{
+    public static class EuclidGCD
+    {
+        public static int Run(int a, int b)
+        {
+            if (b == 0)
+                return a;
+
+            return EuclidGCD.Run(b, a % b);
+        }
+    }
+}
