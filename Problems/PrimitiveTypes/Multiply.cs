77c3c013a825bbf35a190703a02fb0981adbbf28
mmoroney <mikemoroney1722@hotmail.com>
2016-09-28 08:42:31 -0700

    Added Multiply

---
+++ Problems/PrimitiveTypes/Multiply.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utilities;
+
+namespace Problems.PrimitiveTypes
+{
+    [TestClass]
+    public class Multiply
+    {
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    Tests.TestFunctions(i, j, (a, b) => a * b, Multiply.Bitwise);
+                }
+            }
+        }
+
+        private static int Bitwise(int a, int b)
+        {
+            int result = 0;
+
+            while(b > 0)
+            {
+                if ((b & 1) == 1)
+                    result = Multiply.Add(result, a);
+                b >>= 1;
+                a <<= 1;
+            }
+
+            return result;
+        }
+
+        private static int Add(int a, int b)
+        {
+            if (b == 0)
+                return a;
+
+            return Multiply.Add(a ^ b, (a & b) << 1);
+        }
+    }
+}
--- Utilities/Tests.cs
+++ Utilities/Tests.cs
@@ -16,6 +16,17 @@ namespace Utilities
             }
         }
 
+        public static void TestFunctions<T, U, V>(T p1, U p2, params Func<T, U, V>[] functions)
+        {
+            V[] results = new V[functions.Length];
+
+            for (int i = 0; i < results.Length; i++)
+            {
+                results[i] = functions[i](p1, p2);
+                Assert.AreEqual(results[0], results[i]);
+            }
+        }
+
         public static void TestFunctions<T, U, V, W>(T p1, U p2, V p3, params Func<T, U, V, W>[] functions)
         {
             W[] results = new W[functions.Length];
