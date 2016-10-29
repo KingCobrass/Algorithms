766a9f85fa531c13251a2f2ff00505f559632a7e
mmoroney <mikemoroney1722@hotmail.com>
2016-10-29 10:36:43 -0700

    Added binary search.

---
+++ AlgorithmTests/SearchTests/BinarySearchTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Search;
+using Utilities;
+
+namespace AlgorithmTests.SearchTests
+{
+    [TestClass]
+    public class BinarySearchTests
+    {
+        [TestMethod]
+        public void BinarySearchTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 100; j++)
+                {
+                    int[] data = ArrayUtilities.CreateRandomArray(j, -100, 100);
+                    Array.Sort(data);
+
+                    for(int n = -100; n <= 100; n++)
+                    {
+                        int index = BinarySearch.Find(data, n);
+
+                        if (index < 0)
+                            Assert.IsTrue(Array.BinarySearch(data, n) < 0);
+                        else
+                            Assert.AreEqual(n, data[index]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Search/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Search
+{
+    public static class BinarySearch
+    {
+        public static int Find<T>(T[] data, T item)
+            where T : IComparable<T>
+        {
+            int low = 0;
+            int high = data.Length - 1;
+
+            while(low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                int compare = item.CompareTo(data[mid]);
+
+                if (compare == 0)
+                    return mid;
+                else if (compare > 0)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
