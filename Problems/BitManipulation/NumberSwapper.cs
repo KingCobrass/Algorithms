e7f3e0f5b4c4556499089fbaf4608cf897c4f220
mmoroney <mikemoroney1722@hotmail.com>
2016-12-01 11:12:39 -0800

    Added NumberSwapper problem.

---
+++ Problems/BitManipulation/NumberSwapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problems.BitManipulation
+{
+    [TestClass]
+    public class NumberSwapper
+    {
+        // CTCI 16.1
+        [TestMethod]
+        public void NumberSwapperTest()
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                for(int j = 0; j < 20; j++)
+                {
+                    int a = i;
+                    int b = j;
+                    NumberSwapper.Toggle(ref a, ref b);
+                    Assert.AreEqual(a, j);
+                    Assert.AreEqual(b, i);
+                }
+            }
+        }
+
+        private static void Toggle(ref int a, ref int b)
+        {
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
+    }
+}
