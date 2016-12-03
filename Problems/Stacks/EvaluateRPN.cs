44ee34c3f4fdefba2066d2036f783f113cf562a0
mmoroney <mikemoroney1722@hotmail.com>
2016-12-02 16:07:52 -0800

    Added EvaluateRPN problem.

---
+++ Problems/Stacks/EvaluateRPN.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+
+namespace Problems.Stacks
+{
+    [TestClass]
+    public class EvaluateRPN
+    {
+        [TestMethod]
+        public void EvaluateRPNTest()
+        {
+            EvaluateRPN.EvaluateRPNTest(1729, "1729");
+            EvaluateRPN.EvaluateRPNTest(15, "3,4,+,2,*,1,+");
+            EvaluateRPN.EvaluateRPNTest(42, "6,6,1,+,*");
+        }
+
+        private static void EvaluateRPNTest(int expected, string rpn)
+        {
+            Assert.AreEqual(expected, EvaluateRPN.Stack(rpn));
+        }
+
+        private static int Stack(string rpn)
+        {
+            string operators = "+-*/";
+            Func<int, int, int>[] functions = new Func<int, int, int>[]
+            {
+                (x, y) => x + y,
+                (x, y) => x - y,
+                (x, y) => x * y,
+                (x, y) => x / y,
+            };
+
+            string[] tokens = rpn.Split(',');
+            Stack<int> values = new Stack<int>();
+
+            foreach(string token in tokens)
+            {
+                int index = operators.IndexOf(token);
+                if (index == -1)
+                {
+                    values.Push(int.Parse(token));
+                    continue;
+                }
+
+                int y = values.Pop();
+                int x = values.Pop();
+
+                values.Push(functions[index](x, y));
+            }
+
+            return values.Pop();
+        }
+    }
+}
